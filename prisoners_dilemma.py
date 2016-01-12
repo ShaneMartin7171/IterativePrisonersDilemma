12f5167f780c73325980662088f1b399475bcf5b
SUPERHOTline-Miami <geffrank@gmail.com>
2016-01-12 13:33:50 -0600

    bomb drop

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -276,16 +276,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 7:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'The Bomb Dropper'
         else:
-            # use history, opponent_history, score, opponent_score
-            # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
+            if len(opponent_history)==0 or len(opponent_history)==1:
                 return 'c'
-            elif history[-1]=='c' and opponent_history[-1]=='b':
-                return 'b' # betray is they were sucker last time
+            elif opponent_history[-1]=='c' and history[-1]=='c':
+                return 'c'
+            elif opponent_history[-1]=='c' and opponent_history[-2]=='c':
+                return 'b'
             else:
-                return 'c' #otherwise collude
+                return 'b'
 
 
 
